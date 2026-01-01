7f51d424d155d29b4c0bc4a8f84029fd51c11bf5
YassYassId <idrissiy371@gmail.com>
2026-01-01 14:43:33 +0100

    Set Project and Impelement the Discovery & Gateway services

---
+++ discovery-service/src/test/java/com/emplmanagement/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emplmanagement.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
